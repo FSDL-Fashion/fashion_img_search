c5f43b54e5c0d41652a95c381c7f70cafd3b8f42
Jerome Blin <jerome@grover.com>
2022-09-25 10:39:02 +0200

    fix formatting

--- fis/utils/config.py
+++ fis/utils/config.py
@@ -6,4 +6,4 @@ from dotenv import load_dotenv
 load_dotenv()
 
 DIR_ROOT = str(os.getenv("PROJECT_ROOT"))
-DIR_DATA = os.path.join(DIR_ROOT, 'data')
+DIR_DATA = os.path.join(DIR_ROOT, "data")
--- fis/utils/data/download_fashionpedia.py
+++ fis/utils/data/download_fashionpedia.py
@@ -6,14 +6,14 @@ from fis.utils.config import DIR_DATA
 
 
 # Download from S3
-RAW_TRAIN_IMAGES = 'https://s3.amazonaws.com/ifashionist-dataset/images/train2020.zip'
-RAW_VAL_IMAGES = 'https://s3.amazonaws.com/ifashionist-dataset/images/val_test2020.zip'
-RAW_TRAIN_ANNOTATIONS = 'https://s3.amazonaws.com/ifashionist-dataset/annotations/instances_attributes_train2020.json'
-RAW_VAL_ANNOTATIONS = 'https://s3.amazonaws.com/ifashionist-dataset/annotations/instances_attributes_val2020.json'
+RAW_TRAIN_IMAGES = "https://s3.amazonaws.com/ifashionist-dataset/images/train2020.zip"
+RAW_VAL_IMAGES = "https://s3.amazonaws.com/ifashionist-dataset/images/val_test2020.zip"
+RAW_TRAIN_ANNOTATIONS = "https://s3.amazonaws.com/ifashionist-dataset/annotations/instances_attributes_train2020.json"
+RAW_VAL_ANNOTATIONS = "https://s3.amazonaws.com/ifashionist-dataset/annotations/instances_attributes_val2020.json"
 
 # to local disk
-TRAIN_ANNOTATIONS = 'train.json'
-VAL_ANNOTATIONS = 'val.json'
+TRAIN_ANNOTATIONS = "train.json"
+VAL_ANNOTATIONS = "val.json"
 
 
 def download(url: str, target: str) -> None:
@@ -23,12 +23,11 @@ def download(url: str, target: str) -> None:
         url: url to download from.
         target: file or directory to download to.
     """
-
-    print(f'Downloading from {url}')
+    print(f"Downloading from {url}")
 
     # Images
-    if url.split('.')[-1] == 'zip':
-        path, _ = urllib.request.urlretrieve(url=url)
+    if url.split(".")[-1] == "zip":
+        path, _ = urllib.request.urlretrieve(url=url)  # noqa
         with zipfile.ZipFile(path, "r") as f:
             f.extractall(target)
 
@@ -36,7 +35,7 @@ def download(url: str, target: str) -> None:
 
     # Annotations
     else:
-        urllib.request.urlretrieve(url=url, filename=target)
+        urllib.request.urlretrieve(url=url, filename=target)  # noqa
 
 
 def download_fashionpedia(destination_dir: str = DIR_DATA) -> None:
@@ -45,7 +44,6 @@ def download_fashionpedia(destination_dir: str = DIR_DATA) -> None:
     Args:
         destination_dir: directory where the dataset will be saved.
     """
-
     os.makedirs(destination_dir, exist_ok=True)
 
     download(url=RAW_TRAIN_ANNOTATIONS, target=os.path.join(destination_dir, TRAIN_ANNOTATIONS))
